84643f7896a71b6f83f45e7fb62e66ba255248e4
Nick Ford <nick@getpebble.com>
2013-02-06 17:28:59 -0500

    First Pass

---
+++ battery-monitor.py
@@ -0,0 +1,75 @@
+#!/bin/python
+
+import serial
+import time, sys
+
+serial_port = '/dev/ttyUSB0'
+
+ser = serial.Serial()
+
+ser.port = serial_port
+ser.baudrate = 38400
+ser.timeout=0.08
+ser.open()
+
+try:
+  if True:
+    ser.write("*RST\n")
+    time.sleep(5)
+
+
+  if True:
+    ser.write("FUNC CURR:DC\r")
+    time.sleep(0.5)
+    ser.write("TRIG:SOUR BUS\r")
+    time.sleep(0.5)
+    ser.write("TRIG:SOUR?\r")
+    time.sleep(0.5)
+    print(ser.read(100))
+    ser.write("CURR:DC:NPLC 0.1\n")
+    time.sleep(0.5)
+    ser.write("CURR:DC:RANGE 0.05\n")
+    print(ser.read(100))
+
+  delay = 0.05
+  last_time = time.time()
+  count = 0
+  total_charge = 0
+  total_current = 0
+  total_time = 0
+  battery_charge = 140
+
+  while True:
+    time.sleep(delay)
+    ser.write("FETC?;*TRG\r")
+
+    current_time = time.time()
+    time_delta = (current_time - last_time)
+    last_time = current_time
+
+    try:
+      current_ma = float(ser.readline().strip())*1000
+      count = count + 1
+      #charge_mah = current_ma * time_delta / 3600
+
+      total_current = total_current + current_ma
+      total_time = total_time + time_delta
+
+      battery_life = battery_charge / (total_current/count)
+
+      total_charge = (total_current/count) * (total_time/(60*60))
+
+      # print("%f, %f" % (time_delta, current_ma))
+      # print("charge_used: %f mAh\tbattery_life: %f h or %f d" % (total_charge, battery_life, battery_life/24))
+      print("%f %f %f %f" % (current_time,current_ma, total_charge, battery_life))
+      # time.sleep(1)
+    except ValueError:
+      print("oops!")
+except KeyboardInterrupt:
+  time.sleep(1)
+  ser.write("*RST\n")
+  time.sleep(5)
+  ser.close()
+  print("Bye")
+
+ser.close()
