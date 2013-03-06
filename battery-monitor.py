4446da29973a7cc7ced8d029c96b9bb80c3bf863
Nick Ford <nick@getpebble.com>
2013-03-06 12:15:28 -0500

    Latest script for monitoring power

--- battery-monitor.py
+++ battery-monitor.py
@@ -3,7 +3,7 @@
 import serial
 import time, sys
 
-serial_port = '/dev/ttyUSB0'
+serial_port = '/dev/ttyUSB.BK5491B'
 
 ser = serial.Serial()
 
@@ -39,6 +39,7 @@ try:
   total_time = 0
   battery_charge = 140
 
+  # ser.write("*TRG\r")
   while True:
     time.sleep(delay)
     ser.write("FETC?;*TRG\r")
@@ -47,8 +48,9 @@ try:
     time_delta = (current_time - last_time)
     last_time = current_time
 
+    result = ser.readline().strip()
     try:
-      current_ma = float(ser.readline().strip())*1000
+      current_ma = float(result)*1000
       count = count + 1
       #charge_mah = current_ma * time_delta / 3600
 
@@ -62,9 +64,13 @@ try:
       # print("%f, %f" % (time_delta, current_ma))
       # print("charge_used: %f mAh\tbattery_life: %f h or %f d" % (total_charge, battery_life, battery_life/24))
       print("%f %f %f %f" % (current_time,current_ma, total_charge, battery_life))
-      # time.sleep(1)
     except ValueError:
-      print("oops!")
+      print("Oops! Unable to parse response: %s" % (result))
+      ser.write("SYST:ERR?\r")
+      print("Error status: %s" % ser.readline())
+      time.sleep(1.5) # wait for the error to go away
+
+  # ser.write("*TRG\r")
 except KeyboardInterrupt:
   time.sleep(1)
   ser.write("*RST\n")
